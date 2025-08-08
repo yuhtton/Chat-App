314a33e46efca09eba10f65dc9a84871cbed453a
yuhton <chautan1904@gmail.com>
2025-08-08 21:09:07 +0700

    Message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import { connectDB } from './lib/db.js';
-import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
 
 dotenv.config();
 const app = express();
@@ -14,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes); 
 
 app.listen(PORT, () => {
   console.log('Server is running on port:' + PORT);
