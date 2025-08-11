67cc889b6ef10951bcc6588ee9a269356e20cf7f
yuhton <chautan1904@gmail.com>
2025-08-11 22:58:29 +0700

    ProgfilePage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -60,4 +60,18 @@ signup: async (data) => {
     }
   },
 
+    updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
+
 }));
